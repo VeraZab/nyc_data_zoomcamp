fec182f8a1de3ece0dc245df46fef334e08cc291
VeraZab <vera.zabeida@gmail.com>
2023-02-05 12:24:39 -0500

    prefect tries

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,74 +1,54 @@
 import os
 import pandas as pd
 
-from sqlalchemy import create_engine
+from datetime import timedelta
+from prefect import flow, task
+from prefect.tasks import task_input_hash
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-def env_vars_loaded():
-    if os.getenv("POSTGRES_USER") and \
-        os.getenv("POSTGRES_PASSWORD") and \
-        os.getenv("POSTGRES_DB") and \
-        os.getenv("POSTGRES_HOST") and \
-        os.getenv("POSTGRES_PORT"):
-        return True
-    
-    return False
-    
-def load_green_taxi_data(engine):
-    POSTGRES_TABLE = "green_taxi"
-    DATASET_URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-01.csv.gz"
 
-    def clean_data(data):
-        data.lpep_pickup_datetime = pd.to_datetime(data.lpep_pickup_datetime)
-        data.lpep_dropoff_datetime = pd.to_datetime(data.lpep_dropoff_datetime)
-        data.store_and_fwd_flag = data.store_and_fwd_flag.map({"Y": True, "N": False})
-        
+@task(
+    log_prints=True,
+    retries=3,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
+def extract_data(data_url):
+    """Getting data from url"""
 
-    iterator = pd.read_csv(DATASET_URL, iterator=True, chunksize=100000)
-    data = next(iterator)
-    clean_data(data)
-    data.head(n=0).to_sql(name=POSTGRES_TABLE, con=engine, if_exists='replace')
-    data.to_sql(name=POSTGRES_TABLE, con=engine, if_exists='append')
-    
-    while True:
-        try:        
-            data = next(iterator)
-            clean_data(data)
-            data.to_sql(name=POSTGRES_TABLE, con=engine, if_exists='append')
-        except StopIteration:
-            print("Finished ingesting green taxi data into the postgres database")
-            break
+    return pd.read_csv(data_url)
 
-def load_zones_data(engine):
-    POSTGRES_TABLE = "zones"
-    DATASET_URL = "https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
 
-    iterator = pd.read_csv(DATASET_URL, iterator=True, chunksize=100000)
-    data = next(iterator)
-    data.head(n=0).to_sql(name=POSTGRES_TABLE, con=engine, if_exists='replace')
-    data.to_sql(name=POSTGRES_TABLE, con=engine, if_exists='append')
+@task()
+def transform_data(data):
+    """Cleaning columns"""
 
-    while True:
-        try:        
-            data = next(iterator)
-            data.to_sql(name=POSTGRES_TABLE, con=engine, if_exists='append')
-        except StopIteration:
-            print("Finished ingesting zones data into the postgres database")
-            break
+    data.lpep_pickup_datetime = pd.to_datetime(data.lpep_pickup_datetime)
+    data.lpep_dropoff_datetime = pd.to_datetime(data.lpep_dropoff_datetime)
+    data.store_and_fwd_flag = data.store_and_fwd_flag.map({"Y": True, "N": False})
+    return data
 
-def load_db():
-    POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB = os.getenv("POSTGRES_DB")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-    engine = create_engine(f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}')
-    load_green_taxi_data(engine)
-    load_zones_data(engine)
-    
-    
+@task()
+def load_data(data, table_name):
+    """Loading up to database"""
+
+    with SqlAlchemyConnector.load("psql-db") as database_block:
+        engine = database_block.get_connection(begin=False)
+        data.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+        data.to_sql(name=table_name, con=engine, if_exists="append")
+
+
+@flow(name="ETL Taxi Data")
+def etl(month="01", year="2020", color="green"):
+    """This is the main flow"""
+
+    table_name = f"{color}_taxi_trips"
+    data_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year}-{month}.csv.gz"
+    extracted_data = extract_data(data_url)
+    transformed_data = transform_data(extracted_data)
+    load_data(transformed_data, table_name)
+
+
 if __name__ == "__main__":
-    if env_vars_loaded():
-        load_db()
-    else:
-        print("Please add all required env vars to .env file")
+    etl(month="01", year="2020", color="green")
