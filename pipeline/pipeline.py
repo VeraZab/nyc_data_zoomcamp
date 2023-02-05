8208d19bd008bf9c4a4a8c4bc2dfcc5d328ae41d
VeraZab <vera.zabeida@gmail.com>
2023-02-05 15:11:56 -0500

    load data to gcs as parquet file

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 
 from datetime import timedelta
+from pathlib import Path
 from prefect import flow, task
 from prefect.tasks import task_input_hash
-from prefect_sqlalchemy import SqlAlchemyConnector
+from prefect_gcp.cloud_storage import GcsBucket
 
 
 @task(
-    log_prints=True,
     retries=3,
     cache_key_fn=task_input_hash,
     cache_expiration=timedelta(days=1),
@@ -19,35 +19,52 @@ def extract_data(data_url):
     return pd.read_csv(data_url)
 
 
-@task()
-def transform_data(data):
+@task(
+    log_prints=True,
+)
+def transform_data(data, color):
     """Cleaning columns"""
 
-    data.lpep_pickup_datetime = pd.to_datetime(data.lpep_pickup_datetime)
-    data.lpep_dropoff_datetime = pd.to_datetime(data.lpep_dropoff_datetime)
+    if color == "green":
+        data.lpep_pickup_datetime = pd.to_datetime(data.lpep_pickup_datetime)
+        data.lpep_dropoff_datetime = pd.to_datetime(data.lpep_dropoff_datetime)
+    elif color == "yellow":
+        data.tpep_pickup_datetime = pd.to_datetime(data.tpep_pickup_datetime)
+        data.tpep_dropoff_datetime = pd.to_datetime(data.tpep_dropoff_datetime)
+
     data.store_and_fwd_flag = data.store_and_fwd_flag.map({"Y": True, "N": False})
+    print(f"rows: {len(data)}")
     return data
 
 
 @task()
-def load_data(data, table_name):
-    """Loading up to database"""
+def load_locally(data, color, file_name):
+    """Loading up to local storage as parquet file"""
 
-    with SqlAlchemyConnector.load("psql-db") as database_block:
-        engine = database_block.get_connection(begin=False)
-        data.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
-        data.to_sql(name=table_name, con=engine, if_exists="append")
+    path = Path(f"data/{color}/{file_name}.parquet")
+    data.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def load_gcs(path):
+    """Uploading to GCS"""
+
+    gcp_block = GcsBucket.load("taxi-data")
+    gcp_block.upload_from_path(from_path=path, to_path=path)
+    return
 
 
 @flow(name="ETL Taxi Data")
 def etl(month="01", year="2020", color="green"):
     """This is the main flow"""
 
-    table_name = f"{color}_taxi_trips"
-    data_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year}-{month}.csv.gz"
+    file_name = f"{color}_tripdata_{year}-{month}"
+    data_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{file_name}.csv.gz"
     extracted_data = extract_data(data_url)
-    transformed_data = transform_data(extracted_data)
-    load_data(transformed_data, table_name)
+    transformed_data = transform_data(extracted_data, color)
+    path = load_locally(transformed_data, color, file_name)
+    load_gcs(path)
 
 
 if __name__ == "__main__":
